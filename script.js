d8ec6fef4d79532b5d7933994603ffbebd023587
sanskar050 <116196154+sanskar050@users.noreply.github.com>
2024-12-11 21:37:54 +0530

    script.js

--- script.js
+++ script.js
@@ -2,48 +2,32 @@ async function getQuestions() {
     let a = await fetch("https://gist.githubusercontent.com/cmota/f7919cd962a061126effb2d7118bec72/raw/96ae8cbebd92c97dfbe53ad8927a45a28f8d2358/questions.json")
     let ques = await a.json()
 
-    let arrQuestion = []
-    let arrOptionA = []
-    let arrOptionB = []
-    let arrOptionC = []
-    let arrOptionD = []
-    let arrAnswer = []
+    let ranNum = Math.floor(Math.random() * 547)
 
-    let index = 0;
+    let question = ques[ranNum].question;
 
-        let ranNum = Math.floor(Math.random() * 547)
+    let optionA = ques[ranNum].A
+    let optionB = ques[ranNum].B
+    let optionC = ques[ranNum].C
+    let optionD = ques[ranNum].D
 
-        let question = ques[ranNum].question;
-        // console.log(ques[ranNum].question)
-        arrQuestion.push(question)
+    let answer = ques[ranNum].answer
+     
+    document.querySelector(".quiz-Container").innerHTML = `<div class="result"></div>
 
-        let optionA = ques[ranNum].A
-        arrOptionA.push(optionA)
-        let optionB = ques[ranNum].B
-        arrOptionB.push(optionB)
-        let optionC = ques[ranNum].C
-        arrOptionC.push(optionC)
-        let optionD = ques[ranNum].D
-        arrOptionD.push(optionD)
-
-        let answer = ques[ranNum].answer
-        arrAnswer.push(answer)
-        // console.log(arrQuestion[index])
-        document.querySelector(".quiz-Container").innerHTML = `<div class="result"></div>
-
-        <label class="question"><h2>${arrQuestion[index]}</h2>
+        <label class="question"><h2>${question}</h2>
 
         <div class="options">
-            <div><input type="radio" id="A" name="option" value="A"><label for="A">${arrOptionA[index]}</label></div>
-            <div><input type="radio" id="B" name="option" value="B up"><label for="B">${arrOptionB[index]}</label></div>
-            <div><input type="radio" id="C" name="option" value="C"><label for="C">${arrOptionC[index]}</label></div>
-            <div><input type="radio" id="D" name="option" value="D"><label for="D">${arrOptionD[index]}</label></div>
+            <div><input type="radio" id="A" name="option" value="A"><label for="A">${optionA}</label></div>
+            <div><input type="radio" id="B" name="option" value="B up"><label for="B">${optionB}</label></div>
+            <div><input type="radio" id="C" name="option" value="C"><label for="C">${optionC}</label></div>
+            <div><input type="radio" id="D" name="option" value="D"><label for="D">${optionD}</label></div>
         </div>
 
         <div class="submitDiv"><input onclick="check()" type="submit"></div>
 
-        <div class="answer">${arrAnswer[index]}</div>`
-}
+        <div class="answer">${answer}</div>`
+};
 
 function check() {
     btn = document.getElementsByName("option")
