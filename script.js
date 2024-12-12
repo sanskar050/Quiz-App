3e47d6763c4d3d190f8c1b90b572ccc8b16a655e
sanskar050 <116196154+sanskar050@users.noreply.github.com>
2024-12-12 11:24:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let countOfQ = 0, countOfRightAns = 0;
+
 async function getQuestions() {
+    countOfQ++
     document.querySelector(".loader").style.display = "block"
 
     let a = await fetch("https://gist.githubusercontent.com/cmota/f7919cd962a061126effb2d7118bec72/raw/96ae8cbebd92c97dfbe53ad8927a45a28f8d2358/questions.json")
@@ -8,21 +11,16 @@ async function getQuestions() {
         let ranNum = Math.floor(Math.random() * 547)
 
         let question = data[ranNum].question;
-        // console.log(question)
 
         let optionA = data[ranNum].A
-        // console.log(optionA)
         let optionB = data[ranNum].B
-        // console.log(optionB)
         let optionC = data[ranNum].C
-        // console.log(optionC)
         let optionD = data[ranNum].D
-        // console.log(optionD)
 
         let answer = data[ranNum].answer
-        // console.log(answer)
 
-        document.querySelector(".quiz-Container").innerHTML = `<div class="loader"></div>
+        document.querySelector(".quiz-Container").innerHTML = `
+        <div class="loader"></div>
         <div class="result"></div>
 
         <label class="question"><h2>${question}</h2>
@@ -34,13 +32,17 @@ async function getQuestions() {
             <div><input type="radio" id="D" name="option" value="D"><label for="D">${optionD}</label></div>
         </div>
 
-        <div class="submitDiv"><input class="submit" onclick="check()" type="submit"></div>
+        <div class="buttonDiv">
+        <input class="submit" onclick="check()" type="submit">
+        <button class="end" onclick="end()">End</button>
+        </div>
 
         <div class="answer">${answer}</div>`
     }, 3000);
 }
 
 function toggleStart() {
+    document.querySelector(".quiz-Container > h3").innerHTML = "Starting Quiz..."
     document.querySelector(".start").setAttribute("disabled", true)
     document.querySelector(".start").style.backgroundColor = "rgb(196, 135, 253)"
     getQuestions()
@@ -57,14 +59,25 @@ function check() {
     ans = document.querySelector(".answer").innerHTML
     btn.forEach(e => {
         if (e.checked) {
-            console.log("The answer is" + e.value)
             if (e.value === ans) {
-                document.querySelector(".result").innerHTML = "Right Answer<br>Waiting For Next Question... !"
-                toggleSubmit()
+                document.querySelector(".result").innerHTML = "Right Answer<br>Waiting For Next Question..."
+                countOfRightAns++
+                getQuestions()
             }
             else {
                 document.querySelector(".result").innerHTML = "Wrong Answer, Try Again !"
             }
         }
     });
+}
+
+function end() {
+    document.querySelector(".quiz-Container").innerHTML = `
+    <div class="endDiv">
+        <h2>Quiz Summary</h2>
+        <div class="mT-20">
+        <div class="totalQ">Total Attempted Questions: ${countOfQ}</div>
+        <div class="rightAns">Right Answer Given: ${countOfRightAns}</div>
+        </div>
+    </div>`
 }
